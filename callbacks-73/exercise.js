7bed554d456c2759137507eb19cf6b98c978a050
Eleonora <eleonoracasto93@gmail.com>
2023-02-03 12:46:13 +0100

    callback

---
+++ callbacks-73/exercise.js
@@ -0,0 +1,8 @@
+function printAsyncName(callback , name){
+    setTimeout(() => callback(), 1000);
+    setTimeout(() => console.log(name), 2000);  
+}
+
+const hello = () => console.log("Hello!");
+
+printAsyncName(hello, "Spank")
